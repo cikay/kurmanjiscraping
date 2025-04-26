5d1a98f45d91174001d42a057423b50018d8a656
Muzaffer Cıkay <muzaffercky@gmail.com>
2025-04-26 12:55:32 +0300

    Scrapy all section of xelat

--- kurmanjiscraping/spiders/xelat.py
+++ kurmanjiscraping/spiders/xelat.py
@@ -4,16 +4,21 @@ import scrapy
 class XelatSpider(scrapy.Spider):
     name = "xelat"
     allowed_domains = ["xelat.org"]
-    start_urls = ["https://xelat.org/Gotar"]
+    start_urls = ["https://xelat.org/"]
 
     def parse(self, response):
+        sections = response.css(".section-head a::attr(href)").getall()
+        for section in sections:
+            yield scrapy.Request(section, callback=self.parse_section)
+
+    def parse_section(self, response):
         articles_url = response.css(".post-item-image a::attr(href)").getall()
         for url in articles_url:
             yield scrapy.Request(url, callback=self.parse_article)
 
         next_page_url = response.css("li.next a::attr(href)").get()
         if next_page_url:
-            yield scrapy.Request(next_page_url, callback=self.parse)
+            yield scrapy.Request(next_page_url, callback=self.parse_section)
 
     def parse_article(self, response):
         title = response.css("h1.title ::text").get()
@@ -25,6 +30,6 @@ class XelatSpider(scrapy.Spider):
 
         yield {
             "title": title,
-            "content": "".join(filtered).strip(),
+            "content": "\n".join(filtered),
             "url": response.url,
         }
