7a37425228009dca457f334394d7006b23deb112
Muzaffer Cıkay <muzaffercky@gmail.com>
2025-05-27 14:31:47 +0300

    Get file name from cli

--- rows_count.py
+++ rows_count.py
@@ -1,4 +1,19 @@
+import argparse
+
 import pandas as pd
+
+parser = argparse.ArgumentParser(
+    description="Count the number of rows in a CSV file using pandas."
+)
+
+parser.add_argument(
+    "--file-name",
+    type=str,
+    required=True,
+    help="File name",
+)
+
+args = parser.parse_args()
 
 
 def count_rows_with_pandas(file_path):
@@ -6,7 +21,5 @@ def count_rows_with_pandas(file_path):
     return len(df)
 
 
-# Example usage
-file_path = "mezopotamya.csv"
-count = count_rows_with_pandas(file_path)
+count = count_rows_with_pandas(args.file_name)
 print(f"The CSV file contains {count} rows (excluding header).")
